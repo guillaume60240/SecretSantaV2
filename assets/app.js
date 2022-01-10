f60162c517219f15c2e44534f4f94e5f321de5bd
guillaume60240 <77913074+guillaume60240@users.noreply.github.com>
2022-01-10 11:52:21 +0100

    delete bootstrap.vundle.js

--- assets/app.js
+++ assets/app.js
@@ -8,7 +8,9 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.scss';
 
+import { Tooltip, Toast, Popover } from 'bootstrap'
+
+
 // start the Stimulus application
 import './bootstrap';
-import './bootstrap.bundle.js';
 import './inputsValidation';
